dfa637685256de70308275dba2c8dcc93c47fe6e
Rishav Singh <89840621+Rishav-Singh0205@users.noreply.github.com>
2022-09-07 20:34:38 +0530

    Updates

---
+++ precache-manifest.c963cd2efc8c09d7094e6bdaf326ae3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "613112c3ffd1bebb1523",
+    "url": "/inotebook/static/css/main.9fd04ada.chunk.css"
+  },
+  {
+    "revision": "613112c3ffd1bebb1523",
+    "url": "/inotebook/static/js/main.613112c3.chunk.js"
+  },
+  {
+    "revision": "a2f85965cec9ab2c8d37",
+    "url": "/inotebook/static/js/1.a2f85965.chunk.js"
+  },
+  {
+    "revision": "8b3610606df955f110e8",
+    "url": "/inotebook/static/js/runtime~main.8b361060.js"
+  },
+  {
+    "revision": "1c27b256440f6fc9d41b47a8347c2af7",
+    "url": "/inotebook/index.html"
+  }
+];
